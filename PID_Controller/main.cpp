3133e8c87d12b2b7afce10619bd50412f6354e27
Chris Fadden <cfaddenpsu@gmail.com>
2026-07-10 16:42:00 -0400

    Implemented Generic PID controller with Derivative Filtering and Windup Prevention

---
+++ PID_Controller/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "PID.h"
+
+int main(int argc, char **argv){
+  
+  //Create PID Controller
+  
+  struct PID_parameters Quad_PID;
+
+  Quad_PID.Kp = 0.3;
+  Quad_PID.Ki = 0.5;
+  Quad_PID.Kd = 0.01;
+    
+  double set_value = 0.5; 
+
+    //Objective function is (x - 0.5)^2 = x*x - x + 0.25;  Minimum at x = 0.5
+  double processValue = 0.25;
+
+  //Create PID Loop
+  PID_RESET(&Quad_PID);
+  for(int ii = 0; ii < 40; ii++){
+    
+    if( ii % 5 == 0){
+      std::cout << "The process value (should be 0.5) is: " << processValue << std::endl;
+    }
+    processValue = processValue + PID_update(set_value, processValue, &Quad_PID);
+    
+  }
+
+  return 0;
+}
